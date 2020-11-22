2a4d43947578d10431743edb1eb9ff9ac512edcb
Rick Zolnierek <33429254+ZombieAlienRobot@users.noreply.github.com>
2020-11-22 20:53:53 +0100

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,13 @@
+public class Example
+{
+  public static void main(String[] args)
+  {
+      String name = "Adam";
+    System.out.print ("Hello " + name +"\r\n");
+    
+    if(name == "Adam") 
+    {
+    System.out.print("Special user priveledges granted!");
+    }
+  }
+}
